13a03aea4bf737a8c1465eb63818a09d06958ad4
sharadks <sharadeduworld@gmail.com>
2020-11-27 14:33:45 +0530

    initial commit

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: true,
+  application:
+  {
+    name: 'angular-starter',
+    angular: 'Angular 11.0.2',
+    bootstrap: 'Bootstrap 5.0.0',
+    fontawesome: 'Font Awesome 5.15.1',
+  },
+  url: 'http://localhost:5204',
+  config: {
+    /* SELECT ONE OF THOSE CONFIGURATIONS */
+
+    /* LOCAL JSON (NO CRUD) */
+    api: false,
+    url: './assets/params/json/crud/',
+
+    /* LOCAL REST API CRUD  */
+    /* api: true,
+    url: 'http://localhost:5200/', */
+  },
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: false,
+  application:
+  {
+    name: 'angular-starter',
+    angular: 'Angular 11.0.2',
+    bootstrap: 'Bootstrap 5.0.0',
+    fontawesome: 'Font Awesome 5.15.1',
+  },
+  url: 'http://localhost:5004',
+  config: {
+    /* SELECT ONE OF THOSE CONFIGURATIONS */
+
+    /* LOCAL JSON (NO CRUD) */
+     api: false,
+    url: './assets/params/json/crud/',
+
+    /* LOCAL REST API CRUD WITH POSTGRESQL */
+    /* api: true,
+    url: 'http://localhost:5004/', */
+  },
+};
